4594f9673ff31eb029f4faf98fb0424036404165
medmaaster <medamnn@gmail.com>
2022-05-23 15:09:19 +0200

    search file modified

--- 1-Suche.py
+++ 1-Suche.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*
 import requests
 import os
-import re
+import  winsound
+from stem.control import Controller
+from stem import Signal
 from multiprocessing import Pool
 from bs4 import BeautifulSoup
 
@@ -9,6 +11,7 @@ from bs4 import BeautifulSoup
 directory = "1-Suche/"
 if not os.path.exists(directory):
     os.makedirs(directory)
+x = 0
 
 
 url = "https://www.urologenportal.de/patienten/urologensuche.html?tx_urosearch_pi1[action]=list&tx_urosearch_pi1[controller]=Adresse&cHash=5edf1223c9f1fd3874d64d32ed0f192a"
@@ -16,7 +19,24 @@ headers = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36 Edg/95.0.1020.44"
                  ""
 }
+
+
+def get_tor_session():
+    session = requests.session()
+    # Tor uses the 9050 port as the default socks port
+    session.proxies = {'http':  'socks5://127.0.0.1:9050',
+                       'https': 'socks5://127.0.0.1:9050'}
+    return session
+session = get_tor_session()
+# signal TOR for a new connection
+def renew_connection():
+    with Controller.from_port(port = 9051) as controller:
+        controller.authenticate(password="callofdutty23")
+        controller.signal(Signal.NEWNYM)
+
+
 def load_zipcode(plz):
+    global x, session
     url_search = url +str(plz)
     #session = requests.Session()
     filename = plz + ".html"
@@ -33,11 +53,21 @@ def load_zipcode(plz):
         "tx_urosearch_pi1[search][name]": "",
         "tx_urosearch_pi1[search][choice]": ""
         }
-        
-        r = requests.post(url, data=payload,timeout= 20)
+        x += 1
+        if x > 10:
+            print(session.get("http://httpbin.org/ip").text)
+            renew_connection()
+            session = get_tor_session()
+            print(session.get("http://httpbin.org/ip").text)
+            x = 0
+        try :
+            r = session.post(url, data=payload,timeout= 10)
+        except:
+            winsound.Beep(1000,500)
+        #r = requests.post(url, data=payload,timeout= 20)
         #r = requests.get(url_search, headers =headers)
         soup = BeautifulSoup(r.text, "html.parser")
-        print(filename+ "\t"+ str(r.status_code))
+        print(filename+ "\t"+ str(r.status_code) + "\t" + str(x))
         with open(directory + filename,'w+') as f:
             f.write(r.text)
 
@@ -47,5 +77,5 @@ if __name__ == '__main__':
     with open("PLZ_DE.csv", "r") as f:
         zipcodes = [line.replace("\n", "").split(";")[0] for line in f.readlines()[1:]]
 
-    with Pool() as p:
+    with Pool(4) as p:
         p.map(load_zipcode, zipcodes)
